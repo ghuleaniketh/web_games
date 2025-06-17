8139592adbc9e44cf73022c71ade23e21a34244e
Aniketh Ghule <ghuleaniketh@gmail.com>
2025-06-17 22:44:27 +0530

    home is almost done

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,7 +1,28 @@
 import styles from './home.module.css'
 
 export default function Home(){
+    let username = "Aniketh"
     return(
-        <h1>This is home page</h1>
+        <>
+        <div className={styles.topnav}>
+            <div className={styles.username}>
+                <p>Hello {username}</p>
+            </div>
+        </div>
+        <div className={styles.nav}>
+            <div className={styles.branding}>
+                <h1>Wel-Come to Games Snap</h1>
+            </div>
+        </div>
+        <div className={styles.heading1}>
+            <p>Available Games</p>
+        </div >
+        <div className={styles.gamecont}>
+            <div className={styles.gamecont}>
+                <img src='./img/simon.png'/>
+            </div>
+        </div>
+        </>
+        
     )
 }
