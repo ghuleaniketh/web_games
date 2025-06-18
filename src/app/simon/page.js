f504c18cdcd10ab37cc44120e80425c4f125d303
Aniketh Ghule <ghuleaniketh@gmail.com>
2025-06-18 16:57:31 +0530

    simon game is almost done

--- src/app/simon/page.js
+++ src/app/simon/page.js
@@ -1,15 +1,26 @@
 import styles from './simon.module.css';
 
 export default function Simon(){
+    let username = "aniketh";
+    let highscore = 34
     return(
     <>
     <div className={styles.page}>
         <div className={styles.bar}>
             <div className={styles.mainNav}>
-
+                <h1>Let'go {username}</h1>
+                <h2 className={styles.highscore}>Highscore {highscore}</h2>
             </div>
         </div>
         <div className={styles.gamepage}>
+            <div className={styles.status}>
+                <div className={styles.icon}>
+
+                </div>
+                <div className={styles.current}>
+
+                </div>
+            </div>
             <div className={styles.gamecont}>
                 <div className={styles.up}>
                     <div className={styles.red}></div>
