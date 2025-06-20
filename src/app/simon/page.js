189d44bd33880f811218ac1bc5d3f47d07aa5d6e
Aniketh Ghule <ghuleaniketh@gmail.com>
2025-06-20 23:44:34 +0530

    signing off bro

--- src/app/simon/page.js
+++ src/app/simon/page.js
@@ -31,22 +31,49 @@ export default function Simon(){
         let [userPattern,setUserPattern] = useState([]);
         let [gamePattern,setGamePattern] = useState([]);
         let [game,setGameMode] = useState(false);
+        let [heading,setHeading] = useState("Click me to  Start the Game");
+        let [level , setGameLevel] = useState(0);
+        let colors = ['red','green','blue','yellow'];
 
         let startGame = ()=>{
             if(game === false){
-                game = true;
+                setGameMode(true);
                 console.log("game started......");
+                setHeading("Game Started !!!!!!!!!!");
+                setTimeout(inclevel,750);
             }
+        }
+
+        function inclevel(){
+            let Rcolor = getRandomColor();
+            console.log("random color  " +  Rcolor);
+            glow(green);
+            setGamePattern([...gamePattern,Rcolor]);
+            let newLevel = level + 1;
+            setGameLevel(newLevel);
 
         }
         function userClick(event){
             let el = event.target.id;
             console.log(el);
             setUserPattern([...userPattern,el]);
-            useEffect(()=>{
-                console.log(userPattern);
-            },[userPattern]);
         }
+
+        function getRandomColor(){ 
+            let random = Math.floor(Math.random() * 4);
+            return(colors[random]);
+        }
+        
+        function glow(color){
+            console.log("i am getting " + color)
+                let el = document.getElementById(color);
+                console.log(el);
+                        }
+        
+        useEffect(()=>{
+                console.log("user array " + userPattern);
+                console.log("game array " + gamePattern);
+            },[userPattern,gamePattern]);
 
 
     return(
@@ -59,17 +86,16 @@ export default function Simon(){
                 <p className={styles.leaderboard}>Leaderboard</p>
             </div>
         </div>
-        <div className={styles.gamepage} onClick={startGame}>
+        <div className={styles.gamepage} >
             <div className={styles.status}>
                 <div className={styles.icon}>
                     <FontAwesomeIcon icon={faRotateRight} />
                 </div>  
                 <div >
-                    <p className={styles.heading}>adg</p>
-
+                    <p onClick={startGame} className={styles.heading}>{heading}</p>
                 </div>
                 <div className={styles.currentscore}>
-                    <p>Level 4</p>
+                    <p>Level {level}</p>
                 </div>
             </div>
             <div className={styles.gamecont} >
