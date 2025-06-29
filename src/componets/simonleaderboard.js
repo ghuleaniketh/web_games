3d33c3c5a8010860ddb1e8db2ec99010d4f5e8fb
Aniketh Ghule <ghuleaniketh@gmail.com>
2025-06-29 16:40:23 +0530

    making leader board

---
+++ src/componets/simonleaderboard.js
@@ -0,0 +1,11 @@
+'use client'
+import styles from './simonleaderboard.module.css';
+
+
+export default function LeaderBoard(){
+    return(
+        <div className={styles.leaderboard}>
+            
+        </div>
+    )
+}
