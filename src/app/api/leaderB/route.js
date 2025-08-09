24d3c6db62e5d25457be72cedc29d8e07afa5730
aniketh ghule <ghuleaniketh@gmail.com>
2025-08-09 17:54:03 +0530

    solved somne bugs from leaderbored page and made the it some reposnsive

--- src/app/api/leaderB/route.js
+++ src/app/api/leaderB/route.js
@@ -1,7 +1,7 @@
 import db from '@/app/lib/db'
 
 export async function GET(){
-    const q = 'SELECT * FROM users ORDER BY simon_scrore DESC ';
-    const [data] = await db.query(q);
-    return Response.json(data, { status: 200 }); 
+    const q = 'SELECT * FROM users ORDER BY simon_score DESC ';
+    const data = await db.query(q);
+    return Response.json(data[0], { status: 200 }); 
 }
